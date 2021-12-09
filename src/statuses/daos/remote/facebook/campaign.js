31d16fc23ce86e4acf9e3060caf1d520820a148a
edo777 <edgar.khachatryan95@mail.ru>
2021-12-09 19:18:05 +0400

    some bug fix

--- src/statuses/daos/remote/facebook/campaign.js
+++ src/statuses/daos/remote/facebook/campaign.js
@@ -10,43 +10,42 @@ const { APIRequest } = require("../../../../sdks/facebook");
  * }]} data 
  * @returns 
  */
-async function bulkReadAds(sdk, data) {
+function bulkReadAds(sdk, data) {
     if(!sdk) {
         return { status: "failed", result: "sdk is required" };
     }
 
     return new Promise(async (resolve, reject) => {
-            // use batch api
-            const apiBatch = await sdk.apiBatch();
+        // use batch api
+        const apiBatch = await sdk.apiBatch();
 
-            // Create requests for batch execution
-            for (let i = 0; i < data.length; i++) {
-                const { campaignId, adIds, adFields } = data[i];
-                const url = `/${campaignId}/ads`; //?ids=${adIds.split(",")}&fields=${adFields.split(",")}
-                const request = new APIRequest("", "GET", url);
+        // Create requests for batch execution
+        for (let i = 0; i < data.length; i++) {
+            const { campaignId, adIds, adFields } = data[i];
+            const url = `/ads`; //?ids=${adIds.split(",")}&fields=${adFields.split(",")}
+            const request = new APIRequest(campaignId, "GET", url);
 
-                if(adIds && adIds.length) {
-                    request.addParam("ids",  adIds)
-                }
-
-                if(adFields && Object.keys(adFields).length) {
-                    // request.addFields(adFields)
-                    request.addField('status')
-                }
-                        
-                apiBatch.addRequest(
-                    request,
-                    (response) => console.log(response.error, "--------------------"),
-                    (response) => console.log(response.body, "-----------------------")
-                );
+            if(adIds && adIds.length) {
+                request.addParam("ids",  adIds)
             }
 
-            try {
-                const result = await apiBatch.execute();
-                return resolve(result);
-            } catch (error) {
-                return reject(err)
+            if(adFields && Object.keys(adFields).length) {
+                request.addFields(adFields)
             }
+                    
+            apiBatch.addRequest(
+                request,
+                (response) => console.log(response.body),
+                (response) => console.log(response.error)
+            );
+        }
+
+        try {
+            const result = await apiBatch.execute();
+            return resolve(result);
+        } catch (error) {
+            return reject(err)
+        }
     });
 }
 
