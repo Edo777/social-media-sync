d29ba89e68eb2290760a9e1fef5efc0243ce5eca
edo777 <edgar.khachatryan95@mail.ru>
2021-12-09 19:05:59 +0400

    some bug fix

--- src/statuses/daos/remote/facebook/campaign.js
+++ src/statuses/daos/remote/facebook/campaign.js
@@ -10,20 +10,19 @@ const { APIRequest } = require("../../../../sdks/facebook");
  * }]} data 
  * @returns 
  */
-function bulkReadAds(sdk, data) {
+async function bulkReadAds(sdk, data) {
     if(!sdk) {
         return { status: "failed", result: "sdk is required" };
     }
 
-    return new Promise((resolve, reject) => {
-        (async () => {
+    return new Promise(async (resolve, reject) => {
             // use batch api
             const apiBatch = await sdk.apiBatch();
 
             // Create requests for batch execution
             for (let i = 0; i < data.length; i++) {
                 const { campaignId, adIds, adFields } = data[i];
-                const url = `/ads`; //?ids=${adIds.split(",")}&fields=${adFields.split(",")}
+                const url = `${campaignId}/ads`; //?ids=${adIds.split(",")}&fields=${adFields.split(",")}
                 const request = new APIRequest(campaignId, "GET", url);
 
                 if(adIds && adIds.length) {
@@ -31,7 +30,8 @@ function bulkReadAds(sdk, data) {
                 }
 
                 if(adFields && Object.keys(adFields).length) {
-                    request.addFields(adFields)
+                    // request.addFields(adFields)
+                    request.addField('status')
                 }
                         
                 apiBatch.addRequest(
@@ -47,7 +47,6 @@ function bulkReadAds(sdk, data) {
             } catch (error) {
                 return reject(err)
             }
-        })();
     });
 }
 
