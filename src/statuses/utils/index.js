eec49fd2932ac53cac82cc57103989cd73267149
edo777 <edgar.khachatryan95@mail.ru>
2021-12-10 11:42:38 +0400

    ...

--- src/statuses/utils/index.js
+++ src/statuses/utils/index.js
@@ -1,15 +1,5 @@
-const eventEmitters = require("./event-emitter");
-const password = require("./password");
-const token = require("./token");
-const uploadStorage = require("./upload-file");
-const fsActions = require("./fs-actions");
 const EffectiveStatusDetector = require("./detect-effective-status");
 
 module.exports = {
-    ...eventEmitters,
-    password,
-    token,
-    uploadStorage,
-    fsActions,
     EffectiveStatusDetector
 }
