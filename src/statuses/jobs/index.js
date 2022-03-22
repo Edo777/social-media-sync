0615df042667d82cb9af0a3d338debef47ab29c7
edo777 <edgar.khachatryan95@mail.ru>
2022-03-22 13:19:27 +0400

    ... api calls count calculate

--- src/statuses/jobs/index.js
+++ src/statuses/jobs/index.js
@@ -1,3 +1,4 @@
 module.exports = [
     require("./statuses-sync"),
+    require("./reset-api-calls"),
 ];
--- src/statuses/jobs/reset-api-calls.js
+++ src/statuses/jobs/reset-api-calls.js
@@ -1,16 +1,13 @@
 const { LocalCronJobs } = require("../daos");
 const { getCode } = require("./PROCESS_CODES");
 
-/**
- * Call to dao and start statuses sync process for facebook
- */
-async function facebookStatusesSync() {
+async function resetApiCallsCount() {
     const resetApiCallsCode = getCode("CLEAR_API_CALLS");
     await LocalCronJobs.resetApiCallsCount(resetApiCallsCode);
 }
 
 module.exports = function (schedule) {
     schedule("59 * * * *", async function () {
-        facebookStatusesSync();
+        resetApiCallsCount();
     });
 };
